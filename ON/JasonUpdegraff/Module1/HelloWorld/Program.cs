2eb3214f8e45367031cabd78895bfccd26b90f9e
Jason Updegraff <jbsoccer25@hotmail.com>
2020-07-07 15:08:20 -0700

    Hands-on Practice 1

---
+++ ON/JasonUpdegraff/Module1/HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Name = "Jason";
+            int Age = 28;
+            bool isFemale = false;
+            double MyFloat = 10.1;
+            
+            WriteLine(Name);
+            WriteLine(Age);
+            WriteLine(isFemale);
+            WriteLine(MyFloat);
+        }
+    }
+}
