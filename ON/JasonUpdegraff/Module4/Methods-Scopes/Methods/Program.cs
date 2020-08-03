e2295a7b4dfa79a824ad45856939cca2fc613d1d
Jason Updegraff <jbsoccer25@hotmail.com>
2020-08-03 11:51:15 -0700

    HOP04

---
+++ ON/JasonUpdegraff/Module4/Methods-Scopes/Methods/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace Methods
+{
+    class Program
+    {
+        //public static int Adding(int num1, int num2)
+        //{
+        //    int sum = num1 + num2;
+        //    return sum;
+        //}
+        //challenge
+        public static int Remainder(int a, int b)
+        {
+            int Mod = a % b;
+            return Mod;
+        }
+        static void Main()
+        {
+            WriteLine(Remainder(20, 13));
+        }
+    }
+}
---
+++ ON/JasonUpdegraff/Module4/Methods-Scopes/Scopes/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using static System.Console;
+
+namespace Scopes
+{
+    class Program
+    {
+        public static string Str2;
+        static void ReturnString(string Str1)
+        {
+            Str2 = "Programming in C#";
+
+            Write(Str1);
+        }
+        static void Main(string[] args)
+        {
+            ReturnString("CS131 ");
+            WriteLine(Str2);
+        }
+    }
+}
