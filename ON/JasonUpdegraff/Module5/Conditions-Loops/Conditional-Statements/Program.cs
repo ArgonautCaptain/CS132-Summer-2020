479467a690d5ddaf2d1cc2762fb9ebdff472ec58
Jason Updegraff <jbsoccer25@hotmail.com>
2020-08-08 17:57:11 -0700

    HOP05

---
+++ ON/JasonUpdegraff/Module5/Conditions-Loops/Conditional-Statements/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using static System.Console;
+
+namespace Conditions_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            Console.WriteLine("Guess a number (1-10)");
+            int userinput = Convert.ToInt32(Console.ReadLine());
+
+            if (userinput == num)
+            {
+                WriteLine("Your guess was correct!");
+            }
+            //challenge
+            else if (userinput > 10)
+            {
+                WriteLine("Number must be between 1 and 10!!!");
+            }
+            else if (userinput != num)
+            {
+                WriteLine("You guessed wrong. Good luck next time!");
+            }
+        }
+    }
+}
---
+++ ON/JasonUpdegraff/Module5/Conditions-Loops/While-Loop/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using static System.Console;
+
+namespace While_Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 1;
+
+            WriteLine("Guess a number (1-10)");
+
+            while(true)
+            {
+                int userInput = Convert.ToInt32(Console.ReadLine());
+                if (userInput == num)
+                {
+                    WriteLine("Your guess was correct!");
+                    break;
+                }
+                else if(userInput < 1 || userInput > 10)
+                {
+                    WriteLine("That number is not between 1 and 10!");
+                }
+                else
+                {
+                    WriteLine("You guessed wrong. Guess again: ");
+                }
+            }
+        }
+    }
+}
