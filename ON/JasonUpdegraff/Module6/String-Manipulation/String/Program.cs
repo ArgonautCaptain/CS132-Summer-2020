936b67c3ba1db55979bc54e32192a689cae31fcd
Jason Updegraff <jbsoccer25@hotmail.com>
2020-08-15 17:33:36 -0700

    HOP06

---
+++ ON/JasonUpdegraff/Module6/String-Manipulation/String/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Numerics;
+using static System.Console;
+
+namespace String
+{
+    class Program
+    {
+        static void Main()
+        {
+            string Welcome = "Welcome to ";
+            string CourseName = "CS131";
+            string message = Welcome + CourseName;
+
+            for (int i = 0; i < message.Length; i++)
+            {
+                if(i % 2 == 0) //challenge
+                {
+                    Write(message[i]);
+                }    
+            }
+        }
+    }
+}
